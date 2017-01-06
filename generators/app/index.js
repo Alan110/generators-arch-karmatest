03d791d74cb1f9b43779567e68c40add0869e5d0
Alan110 <306880673@qq.com>
2017-01-06 18:33:52 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,42 +4,8 @@ var yeoman = require('yeoman-generator');
 var fs = require('fs');
 
 var baseconfig = yeoman.Base.extend({
-     prompting: function () {
-        return this.prompt([{
-          type    : 'input',
-          name    : 'name',
-          message : 'Your project name',
-          default : this.appname // Default to current folder name
-        }, {
-          type    : 'confirm',
-          name    : 'cool',
-          message : 'Would you like to enable the Cool feature?'
-        }]).then(function (answers) {
-          this.log('app name', answers.name);
-          this.log('cool feature', answers.cool);
-        }.bind(this));
-    },
-    info: function() {
-        this.log(chalk.green(
-            'I am going to build your app!'
-        ));
-    },
-    install : function () {
-         this.npmInstall([
-            'babel-preset-es2015',
-            'babel-plugin-transform-es2015-modules-umd',
-            'chai',
-            'karma',
-            'mocha',
-            'karma-babel-preprocessor',
-            'karma-chai',
-            'karma-chrome-launcher',
-            'karma-coverage',
-            'karma-mocha',
-            'karma-mocha-reporter'
-            ], { 'save-dev': true });
-    },
-    generateBasic: function() {  //按照自己的templates目录自定义
+
+    myStart: function() {  //按照自己的templates目录自定义
 
         var flag = '.es6';
 
@@ -59,6 +25,23 @@ var baseconfig = yeoman.Base.extend({
         }
 
     },
+
+    install : function () {
+         this.npmInstall([
+            'babel-preset-es2015',
+            'babel-plugin-transform-es2015-modules-umd',
+            'chai',
+            'karma',
+            'mocha',
+            'karma-babel-preprocessor',
+            'karma-chai',
+            'karma-chrome-launcher',
+            'karma-coverage',
+            'karma-mocha',
+            'karma-mocha-reporter'
+            ], { 'save-dev': true });
+    },
+
     end: function() {
         this.log(yosay(
             'Your app has been created successfully!'
